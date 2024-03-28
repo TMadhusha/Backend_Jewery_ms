b562d831a53c651d9b1027d33ffe924e0dd29f2e
Abdullah Asfaque <112546546+Abdasfaque@users.noreply.github.com>
2024-03-28 14:17:01 +0530

    Inventory

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/model/Inventory.java
+++ jewelry_ms/src/main/java/jwl/mis/jewelry_ms/model/Inventory.java
@@ -1,4 +1,85 @@
 package jwl.mis.jewelry_ms.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+@Entity
 public class Inventory {
+    @Id
+    @GeneratedValue
+    private Long item_id;
+    private String itemName;
+    private String type;
+    private byte[] image;
+    private Double actualPrice;
+    private String description;
+    private Double sellingPrice;
+    private Boolean availableStock;
+
+    public Long getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(Long item_id) {
+        this.item_id = item_id;
+    }
+
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+
+    public Double getActualPrice() {
+        return actualPrice;
+    }
+
+    public void setActualPrice(Double actualPrice) {
+        this.actualPrice = actualPrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(Double sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public Boolean getAvailableStock() {
+        return availableStock;
+    }
+
+    public void setAvailableStock(Boolean availableStock) {
+        this.availableStock = availableStock;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
 }
