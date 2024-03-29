7b586fdd5e8eb9b982e2de96a540667c6dc5c9a4
Tirasha <1999tirasha@gmail.com>
2024-03-29 16:33:25 +0530

    last

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/controller/CustomerController.java
+++ jewelry_ms/src/main/java/jwl/mis/jewelry_ms/controller/CustomerController.java
@@ -4,9 +4,12 @@ import jwl.mis.jewelry_ms.model.Customer;
 import jwl.mis.jewelry_ms.repository.CustomerRepository;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
@@ -16,5 +19,9 @@ public class CustomerController {
     @PostMapping("/customer")
     Customer newCustomer(@RequestBody Customer newCustomer){
         return customerRepository.save(newCustomer);
+    }
+    @GetMapping("/getcustomer")
+    List<Customer> getAllCustomers(){
+        return customerRepository.findAll();
     }
 }
