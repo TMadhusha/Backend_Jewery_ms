79ab5c1db0de6da448f75ecf2896716f514f658a
Tirasha <1999tirasha@gmail.com>
2024-03-30 22:17:56 +0530

    commit

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/controller/CustomerController.java
+++ jewelry_ms/src/main/java/jwl/mis/jewelry_ms/controller/CustomerController.java
@@ -4,14 +4,12 @@ import jwl.mis.jewelry_ms.model.Customer;
 import jwl.mis.jewelry_ms.repository.CustomerRepository;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class CustomerController {
     @Autowired
     private CustomerRepository customerRepository;
