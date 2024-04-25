2343d88eec761dcfb1bc138338cf28a3fa138c08
Tirasha <1999tirasha@gmail.com>
2024-04-25 16:15:25 +0530

    l

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/repository/OrdersRepository.java
+++
@@ -1,8 +0,0 @@
-package jwl.mis.jewelry_ms.repository;
-
-import jwl.mis.jewelry_ms.model.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdersRepository extends JpaRepository<Orders,Long> {
-
-}
