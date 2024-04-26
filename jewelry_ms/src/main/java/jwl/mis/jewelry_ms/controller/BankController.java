cdc25beabe00167f84c4a70d97cda766ac706ac3
AktharZaem <117557523+AktharZaem@users.noreply.github.com>
2024-04-26 18:19:07 +0530

    backendcheckout

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/controller/BankController.java
+++ jewelry_ms/src/main/java/jwl/mis/jewelry_ms/controller/BankController.java
@@ -19,12 +19,12 @@ public class BankController {
     private BankRepository bankRepository;
 
     @PostMapping("/bank-save")
-    Bank newBank(@RequestBody Bank newBank){
+    Bank newBank(@RequestBody Bank newBank) {
         System.out.println(newBank);
         return bankRepository.save(newBank);
     }
 
-    @PutMapping("/checkout")
+    @PostMapping("/checkout")
     public ResponseEntity<String> checkout(@RequestBody BankCheckoutRequest checkoutRequest) {
         String accountnumber = checkoutRequest.getAccountnumber();
         String backnumber = checkoutRequest.getBacknumber();
@@ -33,21 +33,17 @@ public class BankController {
         // Find the admin by username
         Bank bank = bankRepository.findBankByAccountnumber(accountnumber);
 //balance seyyonum
-//        if (bank == null) {
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid AccountNumber or Backnumber or Cardnumber");
-//        } else {
-//            if (bank.getAccountnumber().equals(accountnumber)) {
-//                return ResponseEntity.ok("Login successful");
-//                if (bank.getBacknumber().equals(backnumber)){
-//                    if (bank.getCardnumber().equals(cardnumber)){
-//                        return ResponseEntity.ok("Details Approved");
-//                    }
-//                }
-//            } else {
-//                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
-//            }
-//        }
-//    }
-//
-        return null;
-    }}
+        if (bank == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid AccountNumber or Backnumber or Cardnumber");
+        } else {
+            if (bank.getAccountnumber().equals(accountnumber)&&bank.getBacknumber().equals(backnumber)&&bank.getCardnumber().equals(cardnumber)) {
+                //return ResponseEntity.ok("Login successful");
+                        return ResponseEntity.ok("Details Approved");
+
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+            }
+        }
+    }
+
+}
