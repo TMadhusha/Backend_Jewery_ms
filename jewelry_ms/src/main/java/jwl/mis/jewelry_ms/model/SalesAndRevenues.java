3e1e321b1f1611b7e0c5668fbf50e37c4b6f2c5c
TMadhusha <madhusha200069@gmail.com>
2024-05-20 00:39:11 +0530

    l

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/model/SalesAndRevenues.java
+++ jewelry_ms/src/main/java/jwl/mis/jewelry_ms/model/SalesAndRevenues.java
@@ -1,4 +1,28 @@
 package jwl.mis.jewelry_ms.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Entity
+@Getter
+@Setter
 public class SalesAndRevenues {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+    private LocalDate date;
+    @ManyToOne
+    @JoinColumn(name = "cus_id")
+    private Customer customer;
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Inventory inventory;
+    private Long qty;
+    private double unitPrice;
+    private double totalSalesAmount;
+    private double paidAmount;
+    private double balance;
+    private String status;
 }
