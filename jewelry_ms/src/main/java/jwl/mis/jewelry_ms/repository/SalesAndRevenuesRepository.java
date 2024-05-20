7b0110ba8a57b37e9d2771a0e34a8c93c5d3c5a0
TMadhusha <madhusha200069@gmail.com>
2024-05-20 20:58:24 +0530

    last

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/repository/SalesAndRevenuesRepository.java
+++
@@ -1,7 +0,0 @@
-package jwl.mis.jewelry_ms.repository;
-
-import jwl.mis.jewelry_ms.model.SalesAndRevenues;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalesAndRevenuesRepository extends JpaRepository<SalesAndRevenues,Long> {
-}
