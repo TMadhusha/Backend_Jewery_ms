ee526222e2c91ebd4f9df327a7f6484bb15c00d5
TMadhusha <madhusha200069@gmail.com>
2024-07-05 08:28:17 +0530

    Update Expense.java

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/model/Expense.java
+++ jewelry_ms/src/main/java/jwl/mis/jewelry_ms/model/Expense.java
@@ -1,4 +1,20 @@
 package jwl.mis.jewelry_ms.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Entity
+@Getter
+@Setter
 public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long expenseId;
+    private Date date;
+    private String description;
+    private double amount;
+
+
 }
