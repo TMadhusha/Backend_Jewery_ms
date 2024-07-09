62f9ff8ce46b24d17e266eda6d27eb78531784c1
AktharZaem <117557523+AktharZaem@users.noreply.github.com>
2024-07-09 23:03:36 +0530

    last

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/controller/AdminController.java
+++ jewelry_ms/src/main/java/jwl/mis/jewelry_ms/controller/AdminController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -87,6 +88,28 @@ public class AdminController {
         }
     }
 
+    // Update Admin Password
+    @PutMapping("/forgetpwd")
+    public ResponseEntity<String> updatePassword(@RequestBody Map<String, String> requestBody) {
+        try {
+            String username = requestBody.get("username");
+            String newPassword = requestBody.get("password");
+
+            // Find the admin by username
+            Admin admin = adminRepository.findAdminByUsername(username);
+
+            if (admin == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username is Incorrect");
+            } else {
+                // Update password and save
+                admin.setPassword(newPassword);
+                adminRepository.save(admin);
+                return ResponseEntity.ok("Password updated successfully");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating password: " + e.getMessage());
+        }
+    }
 
 
 }
