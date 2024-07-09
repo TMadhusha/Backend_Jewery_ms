8d0adc7af225fc352fd021ce9b3c400135670f2a
TMadhusha <madhusha200069@gmail.com>
2024-07-10 01:40:14 +0530

    Update CartRepository.java

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/repository/CartRepository.java
+++ jewelry_ms/src/main/java/jwl/mis/jewelry_ms/repository/CartRepository.java
@@ -3,5 +3,8 @@ package jwl.mis.jewelry_ms.repository;
 import jwl.mis.jewelry_ms.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartRepository extends JpaRepository<Cart,Long> {
+    List<Cart> findByUsername(String username);
 }
